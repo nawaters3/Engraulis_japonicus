6cd556eca8864dd04cff6541dfad6497de97f8bd
nawaters3 <46134700+nawaters3@users.noreply.github.com>
2021-07-09 11:51:04 +0900

    Final Run for Master's Thesis
    Cleaned up old mistakes. Still lots of room for improvement but 'good enough' to at least discuss results.

--- run_Engraulis_japonicus.m
+++ run_Engraulis_japonicus.m
@@ -6,10 +6,10 @@ check_my_pet(pets);
 
 estim_options('default'); 
 estim_options('max_step_number', 5e3); 
-estim_options('max_fun_evals', 5e2); 
+estim_options('max_fun_evals', 5e3); 
 
 estim_options('pars_init_method', 2); 
-estim_options('results_output', 3); 
+estim_options('results_output', 6); 
 estim_options('method', 'no'); 
 
 estim_pars; 
